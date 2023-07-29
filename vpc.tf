3feb7a021c81677a8f29cd832af3cae6352520c4
Baris Yuksel <baris.yuksel@zivver.com>
2023-07-29 15:41:36 +0200

    nat gateway for private subnet

--- vpc.tf
+++ vpc.tf
@@ -10,6 +10,7 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "public" {
   vpc_id     = aws_vpc.main.id
   cidr_block = var.public_subnet_cidr_block
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "public"
@@ -50,4 +51,35 @@ resource "aws_route_table" "public_rt" {
 resource "aws_route_table_association" "public_subnet_public_rt_assoc" {
   subnet_id = aws_subnet.public.id
   route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_eip" "nat_public_ip" {
+  vpc = true
+}
+
+resource "aws_nat_gateway" "main_natgw" {
+  allocation_id = aws_eip.nat_public_ip.id
+  subnet_id = aws_subnet.public.id
+
+  tags = {
+    Name = "nat gateway"
+  }
+}
+
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.main_natgw.id
+  }
+
+  tags = {
+    Name = "private rt"
+  }
+}
+
+resource "aws_route_table_association" "private_subnet_private_rt_assoc" {
+  subnet_id = aws_subnet.private.id
+  route_table_id = aws_route_table.private_rt.id
 }
